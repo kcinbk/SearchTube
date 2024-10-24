b71c94129d050ec954ce550e8a4cb3ca1eeb29f2
Keenan Chen <110057425+kcinbk@users.noreply.github.com>
2024-10-24 18:38:45 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="SearchTube",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
+    author='Keenan Chen',
     install_requires=[
-        'pandas>=1.0.0',
-        'numpy>=1.18.0',
+        'pandas',
+        'numpy',
         'google-api-python-client>=2.112.0', 
-        'requests>=2.20.0',  
-        'python-dotenv>=1.0.0', 
-    ],
-)
+        'requests',  
+        'python-dotenv' 
+                     ]
+    )
